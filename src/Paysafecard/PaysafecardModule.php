7bca2bf44b83de34547b9b32d6102d352a7d6735
Martindev <Delebecque.martin@gmail.com>
2021-05-20 18:51:03 +0200

    Update PaysafecardModule.php

--- src/Paysafecard/PaysafecardModule.php
+++ src/Paysafecard/PaysafecardModule.php
@@ -33,8 +33,8 @@ class PaysafecardModule extends Module
             $renderer->addPath('paysafecard_admin', __DIR__ . '/Views');
             $router->group($container->get('admin.prefix') . '/paysafecard', 'paysafecard.admin')
                 ->get('', AdminPaysafecardIndexAction::class, 'index')
-                ->post('/[i:id]', PaysafecardActiveIndexAction::class, 'accept')
-                ->delete('/[i:id]', PaysafecardActiveIndexAction::class, 'refuse');
+                ->post('/[i:id]', PaysafecardIndexAction::class, 'accept')
+                ->delete('/[i:id]', PaysafecardIndexAction::class, 'refuse');
         }
     }
 }
